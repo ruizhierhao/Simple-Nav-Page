d2e6ce1653fdd5f21cc673b63c322b0fb3603c57
ruizhierhao <130334722+ruizhierhao@users.noreply.github.com>
2026-08-24 10:52:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 
 // ── 图标 & 背景 配置 ────────────────────────────────────────
 const FAVICON_PROVIDER = 'google';
-const PROXY = 'https://dh.884785.xyz/';
+const PROXY = 'https://dh.884785.xyz';
 
 function withProxy(originUrl) {
   if (!PROXY) return originUrl;
